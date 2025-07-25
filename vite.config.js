2520a8f94dde0c42e5c108c3600138b880cce414
Sharath M <youremail@example.com>
2025-07-26 03:54:21 +0530

    Updated: latest changes to portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/sharath-react-portfolio1/' : '/',
   plugins: [react()],
 })
